ba2b29ea824dee4271ca6c2c1d8c91f21d6ead78
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-13 06:22:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,11 @@
 #write your code here
 
+def countdown(number)
   x == 10
   while x > 0
   x -= 1
- 
+ puts "#{number} SECOND(S)!"
  end
-  puts "#{x} SECOND(S)!"
-
+  puts "HAPPY NEW YEAR!"
+  
+end
